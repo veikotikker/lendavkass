9ddfe178d66adcc2dea8add6a274f99cbae36b5f
veikotikker <128815309+veikotikker@users.noreply.github.com>
2023-04-04 18:56:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
             <h1>Ettevaatust, siit tuleb lendav kass!</h1>
             <p>
               Siit ta tuleb:
-              <img src=\"https://i.pinimg.com/originals/5d/49/83/5d498372870466ba95d776faf5b8f8fd.jpg" />
+              <img src="https:"//i.pinimg.com/originals/5d/49/83/5d498372870466ba95d776faf5b8f8fd.jpg">
             </p>
           </body>
         </html>";
